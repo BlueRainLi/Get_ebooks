5f00a08e9823cb34969ee2e7a9e0fe334c9f0769
BlueRainLi <820924380@qq.com>
2020-02-04 22:39:52 +0800

    Add feature to choose creating single file or multiple files.

--- README.md
+++ README.md
@@ -2,5 +2,15 @@
 This is a small project to get e-books in the form of epub from a novel webesites.
 ```python
 get_title_list(timeout=(8,10),max_retries=5) # To get a title list on wenku8.
-get_single_one(str,timeout=(8,10),max_retries=5) # To get a epub file with an input of number of the novel you choosed.
+get_ebooks(No,booknumber=1,timeout=(8,10),max_retries=5,always=False)
+"""
+A little function to get ebook(s) from www.wenku8.net.
+
+Args:
+No: A string of number representing the book. Example: "1","1999"
+booknumber: A int to choose to create only one file(1) or multiple files(2).
+timeout: A tuple with two values which refer to the waiting time of sending requests and waiting responses
+max_retries: A int to set the times of retring requests the url.
+always: A Boolean to depend whether keep retring to request or not.
+"""
 ```
--- get_ebooks_from_wenku8.py
+++ get_ebooks_from_wenku8.py
@@ -1,7 +1,16 @@
 # -*- coding:utf-8 -*-
+###########################
+#File Name: get_ebooks_from_wenku8.py
+#Author: BlueRainLi
+#Mail: 118010159@link.cuhk.edu.cn
+###########################
+
+import os
+import platform
 import re
 import threading
 import time
+from functools import reduce
 
 import requests
 from bs4 import BeautifulSoup
@@ -9,38 +18,95 @@ from ebooklib import epub
 from requests.adapters import HTTPAdapter
 
 
-def retry_request_get(url,timeout):
+def retry_request_get(url,timeout,always=False):
     ask = 'y' # At least try to request for one times.
-    while ask == 'y':
+    while ask == 'y' or always == True:
         try:
             req = requests.get(url,timeout=timeout)
         except:
-            ask = input('Something went wrong with the network. Do you want to retry? (Y/N) ').lower()
+            if always == False:
+                ask = input('Something went wrong with the network. Do you want to retry? (Y/N) ').lower()
             while ask != 'y' and ask != 'n':
                 ask = input('Please input the correct message. (Y/N)').lower()
         finally:
             if 'req' in locals().keys(): # If req is defined, return req.
                 ask = 'n'
+                always = False
     return req
 
 
-def find_all (c,s:list):
+def find_all(c, s: list):
     if c not in s:
         return False
     return [x for x in range(len(s)) if c == s[x]]
 
 
-def get_picture(index,src,timeout,imglist):
+def get_picture(index:int, src:str, timeout, imglist:list):
     try:
         req = requests.get(src,timeout=timeout)
     except:
         pass
     else:
         imgdata = req.content
-        match = re.search('[0-9]+.(jpg|png)',src)
+        match = re.search('[0-9]+.(jpg|png)', src)
         name = match.group()
         img = epub.EpubItem(file_name=name,media_type=match.group(1),content=imgdata)
         imglist[index] = img
+
+
+def checking_valid(group:list):
+    for item in group:
+        if type(item) != type([]):
+            return False
+        elif item[0].get("class") != ["vcss"]:
+            return False
+    return True
+
+
+def make_dir(name:str):
+    name = name.strip() # Remove white space
+    name = name.rstrip("\\") # Remove \\
+    exist = os.path.exists(name)
+    if not exist:
+        os.makedirs(name)
+        print("Folder "+name+" created.")
+        return True
+    else:
+        print("Folder "+name+" existed.")
+        return False
+
+
+def addlist(x:list,y:list):
+    return x+y
+
+
+def checking_group_valid(group:list):
+    for item in group:
+        if type(item) != type([]):
+            return False
+        elif item[0].get("class") != ["vcss"]:
+            return False
+        else:
+            for i in range(1,len(item)):
+                if item[i].get("class") != ["ccss"]:
+                    return False
+    return True
+
+
+def book_init(name:str,author:str,title=None,series=None,number=None):
+    book = epub.EpubBook()
+    book.set_identifier('id123456')
+    if title == None:
+        book.set_title(name)
+    else:
+        book.set_title(name+' '+title)
+    book.set_language('zh')
+    book.add_author(author)
+    if series != None:
+        book.add_metadata("DC","series",series)
+    if number != None:
+        book.add_metadata("DC","series-index",number)
+    return book
 
 
 def get_title_list(timeout=(8,10),max_retries=5):
@@ -50,13 +116,13 @@ def get_title_list(timeout=(8,10),max_retries=5):
 
     # Set max retries
     s = requests.Session()
-    s.mount('http://',HTTPAdapter(max_retries=max_retries))
-    s.mount('https://',HTTPAdapter(max_retries=max_retries))
+    s.mount('http://', HTTPAdapter(max_retries=max_retries))
+    s.mount('https://', HTTPAdapter(max_retries=max_retries))
 
     # Set main process
     late = 0
     title_list = []
-    for x in range(1,3):
+    for x in range(0,3):
         for i in range(0,1000):
             url = "https://www.wenku8.net/novel/"+str(x)+"/"+str(x*1000+i)+"/index.htm"
             data = BeautifulSoup(retry_request_get(url,timeout).content,"html.parser")
@@ -72,14 +138,23 @@ def get_title_list(timeout=(8,10),max_retries=5):
                 title_list.append(url+"  "+data.find(id='info').string[3:]+"  "+title_data.string)
 
 
-    with open("title_list.txt","w") as f:
+    # Write the data into txt file
+    with open("./title_list.txt","w") as f:
         f.write("\n".join(title_list))
     time2 = time.time()-time1
     print('Done\nUsing time: %0.2fs' %time2)
 
-
-def get_single_one(No,timeout=(8,10),max_retries=5):
-
+def get_ebooks(No:str,booknumber=1,timeout=(8,10),max_retries=5,always=False):
+    """
+    A little function to get ebook(s) from www.wenku8.net.
+    
+    Args:
+    No: A string of number representing the book. Example: "1","1999"
+    booknumber: A int to choose to create only one file(1) or multiple files(2).
+    timeout: A tuple with two values which refer to the waiting time of sending requests and waiting responses
+    max_retries: A int to set the times of retring requests the url.
+    always: A Boolean to depend whether keep retring to request or not.
+    """
     # Start recording time
     time1 = time.time()
 
@@ -88,65 +163,87 @@ def get_single_one(No,timeout=(8,10),max_retries=5):
     s.mount('http://',HTTPAdapter(max_retries=max_retries))
     s.mount('https://',HTTPAdapter(max_retries=max_retries))
 
-    # Set the standard website
-    url = "https://www.wenku8.net/novel/1/"+No+"/index.htm"
-    data = BeautifulSoup(retry_request_get(url,timeout).content,"html.parser") # analyze the data
-    url_link = data.findAll(class_=['vcss','ccss']) # Get the all url link
+    # Set the standard website and get basic data
+    url = "https://www.wenku8.net/novel/"+str(int(No)//1000)+'/'+No+"/index.htm"
+    res = requests.get(url)
+    print("Requests url:",url)
+    print("Requests status:",res.status_code)
+    data = BeautifulSoup(res.content,"html.parser")
+    chapter_list = data.findAll(class_="vcss")
+    url_link = data.findAll(class_=["vcss","ccss"])
+    group = []
+    part = []
+    bookname = data.find(id='title').string
+    author = data.find(id='info').string[3:]
+    print(bookname,author)
 
-    # Create a book and add some basic message
-    book = epub.EpubBook()
-    book.set_identifier('id123456')
-    book.set_title(data.find(id='title').string)
-    book.set_language('en')
-    book.add_author(data.find(id='info').string[3:])
-    print(data.find(id='title').string,data.find(id='info').string)
-
-    # Create toc and contents of book
-    chapters = []
-    toc,x = [],[1,[]]
+    # Create all the data of sub-books
+    print("Volume(s):")
     for i in range(len(url_link)):
         item = url_link[i]
-        if item.get('class') == ['vcss']:
+        if item.get("class") == ["vcss"]:
+            if item != url_link[0]:
+                group.append(part)
+            part = [item]
             print(item.string)
-            if x != [1,[]]:
-                x[1] = tuple(x[1])
-                toc.append(tuple(x))
-            x = [epub.Section(item.string),[]]
+        elif i == len(url_link)-1:
+            part.append(item)
+            group.append(part)
         else:
-            ahref = item.find('a')
+            part.append(item)
+    print("Volume(s) status:",checking_group_valid(group))
+    print("Volume(s) length:",len(group))
+
+
+    # Start collecting the data
+    toc = [None]*(len(group))
+    bookadd = [None]*(len(group))
+    for j in range(len(group)):
+        item = group[j]
+        print(j+1,item[0].string)
+        toc[j] = [epub.Section(item[0].string),[]]
+        chapters = []
+        imglist = None
+        for i in range(1,len(item)):
+            ahref = item[i].find('a')
             if ahref != None:
                 filename = re.match('[0-9]+',ahref.get('href')).group()
                 chapter = epub.EpubHtml(title=ahref.string,file_name=filename+'.xhtml',lang='hr')
-                innerlink = "https://www.wenku8.net/novel/"+str(int(No)//1000)+'/'+No+"/"+ahref.get("href")
-                print(filename,item.string)
-                text_data = BeautifulSoup(retry_request_get(innerlink,timeout).content,"html.parser")
-                if item.string != '插图': # Get basic pages
-                    text = '<br/>'+item.string+'<br/>'+str(text_data.find(id='content'))
+                innerlink = "https://www.wenku8.net/novel/"+str(int(No)//1000)+'/'+No+'/'+ahref.get('href')
+                print("  %5s"%filename,item[i].string)
+                text_data = BeautifulSoup(retry_request_get(innerlink,timeout,always=always).content,"html.parser")
+                if item[i].string != '插图':
+                    # Get basic pages
+                    text = '<br/>'+item[i].string+'<br/>'+str(text_data.find(id='content'))
                     text = text.replace('<br/>\n<br/>','<br/>')
                     chapter.set_content(text)
-                    x[1].append(chapter)
+                    toc[j][1].append(chapter)
                     chapters.append(chapter)
-                    book.add_item(chapter)
-                else: # Get picture pages
+                else:
+                    # Get picture pages
                     imglink = text_data.findAll('img',class_='imagecontent')
                     lens = len(imglink)
-                    print('Number of images:',lens)
+                    print('  Number of picture(s):',lens)
                     imglist = [None]*lens
                     while find_all(None,imglist):
                         Nonelist = find_all(None,imglist)
                         pendinglist = []
+                        ask = 'y'
                         if len(Nonelist) != lens:
-                            ask = input('%d picture(s) failed. Do you want to retry? (Y/N) ' % len(Nonelist)).lower()
+                            if always == False:
+                                ask = input('  %d picture(s) failed. Do you want to retry? (Y/N) ' % len(Nonelist)).lower()
+                            else:
+                                print('  %d picture(s) failed. Retring.' % len(Nonelist))
                             if ask != 'y':
                                 break
                         for i in Nonelist:
-                            item = imglink[i]
-                            src = item.get('src')
+                            k = imglink[i]
+                            src = k.get('src')
                             pendinglist.append(threading.Thread(target=get_picture,args=(i,src,timeout,imglist)))
-                        for item in pendinglist:
-                            item.start()
-                        for item in pendinglist:
-                            item.join()
+                        for x in pendinglist:
+                            x.start()
+                        for x in pendinglist:
+                            x.join()
                     text = '<img src='
                     for i in range(lens):
                         name = imglist[i].get_name()
@@ -155,30 +252,42 @@ def get_single_one(No,timeout=(8,10),max_retries=5):
                         else:
                             text = text + name + '>'
                     chapter.set_content(text)
-                    x[1].append(chapter)
                     chapters.append(chapter)
-                    book.add_item(chapter)
-                    for item in imglist:
-                        book.add_item(item)
-            if i == len(url_link)-1: # When finish all the link, add them to toc.
-                x[1] = tuple(x[1])
-                if tuple(x) not in toc:
-                    toc.append(tuple(x))
+                    toc[j][1].append(chapter)
+        if imglist == None:
+            bookadd[j] = chapters
+        else:
+            bookadd[j] = chapters + imglist
+    
 
-    # Add important data such as toc and spine
-    book.toc = tuple(toc)
-    book.spine = ['nav']+chapters
-    book.add_item(epub.EpubNcx())
-    book.add_item(epub.EpubNav())
+    if booknumber == "2":
+        make_dir(bookname)
+        for i in range(len(chapter_list)):
+            book = book_init(bookname,author,title=chapter_list[i].string)
+            book.toc = tuple(toc[i][1])
+            book.spine = ['nav']+bookadd[i]
+            for item in bookadd[i]:
+                book.add_item(item)
+            book.add_item(epub.EpubNcx())
+            book.add_item(epub.EpubNav())
+            
+            epub.write_epub(bookname+"/"+bookname+" "+chapter_list[i].string+".epub",book)
+    else:
+        spine = reduce(addlist,bookadd)
+        book = book_init(bookname,author)
+        book.toc = tuple(toc)
+        book.spine = ['nav']+spine
+        for item in spine:
+            book.add_item(item)
+        book.add_item(epub.EpubNcx())
+        book.add_item(epub.EpubNav())
 
-    # Write the books
-    epub.write_epub(data.find(id='title').string+'.epub',book)
-
-    # Return the time
+        epub.write_epub(bookname+".epub",book)
+    
     time2 = time.time()-time1
-    print('Done\nUsing time: %0.2fs' %time2)
+    print('Done.\nUsing time: %0.1f min' %(time2/60))
+
 
 
 if __name__ == "__main__":
-    # get_title_list()
-    get_single_one('1999')
+    get_ebooks("1999",booknumber=2)
